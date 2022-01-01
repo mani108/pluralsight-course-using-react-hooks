c7ed3cc0348cff7cf4992e6938466750cc2b9f7e
mani <cspuntambekar@gmail.com>
2022-01-01 18:34:47 +0530

    T1

--- 02-Basic-React-Hooks-useState-useRef-useEffect/clip2-Setting-Up-Toolchain/pages/index.js
+++ 02-Basic-React-Hooks-useState-useRef-useEffect/clip2-Setting-Up-Toolchain/pages/index.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React,{useState} from "react";
 
 const InputElement = () => {
-  return <input placeholder="Enter Some Text" />;
+const [inputText, setInputText] = useState("");
+
+  return <div><input 
+  onChange={(e) => {
+    setInputText(e.target.value)
+  }}
+  placeholder="Enter Some Text" /><br/>{inputText}</div>;
 };
 
 export default InputElement;
